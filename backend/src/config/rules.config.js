ad7327f122bd1e16bc7d99233680f4d68bb9bc84
Francesco La Barba <87284271+Fralas@users.noreply.github.com>
2025-12-04 21:54:01 +0100

    update - config armi

--- backend/src/config/rules.config.js
+++ backend/src/config/rules.config.js
@@ -13,29 +13,25 @@ export const missionRules = {
   // These are air-to-air missiles, guided bombs, and heavy ordnance for planes
   importantWeaponsAirports: [
     // Air-to-air missiles for jets
-    'weapons.missiles.P_77',
-    'weapons.missiles.P_73',
-    'weapons.missiles.P_27PE',
-    'weapons.missiles.PL-PL_5EII',
-    'weapons.missiles.SD-10',
-    'weapons.missiles.AIM_120',
     'weapons.missiles.AIM_120C',
     'weapons.missiles.AIM_9X',
-    'weapons.missiles.AIM_9',
     'weapons.missiles.AIM_54C_Mk60',
     'weapons.missiles.P_60',
-    'weapons.missiles.R-60',
+    'weapons.missiles.AIM_7',
 
     // Air-to-ground missiles for jets
     'weapons.missiles.AGM_65D',
+    'weapons.missiles.AGM_65F',
     'weapons.missiles.AGM_65H',
     'weapons.missiles.AGM_65L',
-    'weapons.missiles.AGM_65F',
     'weapons.missiles.AGM_88',
     'weapons.missiles.AGM_154A',
-    'weapons.missiles.X_29T',
-    'weapons.missiles.LD-10',
-    'weapons.missiles.Kh25MP_PRGS1VP',
+    'weapons.missiles.AGM_154',
+    'weapons.missiles.ADM_141A',
+    'weapons.missiles.LAU_61_APKWS_M282',
+    'weapons.missiles.AGM_84D',
+
+    //PODS
 
     // Guided bombs for jets
     'weapons.bombs.GBU_12',
@@ -45,12 +41,10 @@ export const missionRules = {
     'weapons.bombs.GBU_38',
     'weapons.bombs.GBU_31_V_3B',
     'weapons.bombs.GBU_31',
-    'weapons.missiles.GB-6-SFW',
-    'weapons.missiles.LS_6_500',
-    'weapons.bombs.LS_6_100',
 
     // Cluster and unguided bombs for jets
     'weapons.bombs.CBU_105',
+    'weapons.bombs.CBU_103',
     'weapons.bombs.CBU_97',
     'weapons.bombs.Mk_82',
     'weapons.bombs.Mk_84',
@@ -66,12 +60,6 @@ export const missionRules = {
     'weapons.missiles.AGM_114',
     'weapons.missiles.Ataka_9M120',
     'weapons.missiles.Ataka_9M120F',
-    'weapons.missiles.TOW',
-    'weapons.missiles.HOT3_MBDA',
-    'weapons.missiles.SPIKE_ER',
-    'weapons.missiles.SPIKE_ER2',
-    'weapons.missiles.HJ-12',
-
     // Rocket pods for helicopters
     'weapons.nurs.HYDRA_70_M151',
     'weapons.nurs.HYDRA_70_M229',
@@ -82,16 +70,13 @@ export const missionRules = {
     'weapons.nurs.C_13',
     'weapons.nurs.S_5M',
     'weapons.nurs.S-24B',
+    'weapons.missiles.AGR_20A',
+    'weapons.missiles.AGR_20_M282',
 
     // Air-to-air missiles for helicopters
     'weapons.missiles.Igla_1E',
-    'weapons.missiles.Mistral',
     'weapons.missiles.OH58D_FIM_92',
-    'weapons.missiles.R_550',
-
-    // Light AGMs that can be used by helicopters
-    'weapons.missiles.AGR_20A',
-    'weapons.missiles.AGR_20_M282',
+    'weapons.missiles.P_60',
   ],
 
   // Liquids configuration
