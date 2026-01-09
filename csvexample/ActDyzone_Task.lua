d890c4fd6475ccd1dfbeff32bcc7a28835efa0aa
Francesco La Barba <87284271+Fralas@users.noreply.github.com>
2026-01-09 20:05:18 +0100

    Add - test file

---
+++ csvexample/ActDyzone_Task.lua
@@ -0,0 +1,12 @@
+-- Auto-generated each refresh (ACTIVE dynamic zones) - same as labels
+
+zone_31 : SEAD
+zone_36 : CAS
+zone_37 : SEAD, DEAD, CAS
+zone_38 : SEAD, DEAD, CAS
+zone_39 : SEAD, DEAD, CAS
+zone_40 : SEAD, DEAD, CAS
+zone_42 : SEAD, DEAD, CAS
+zone_43 : SEAD, DEAD, CAS
+zone_44 : SEAD, DEAD, CAS
+zone_45 : SEAD, DEAD, CAS
